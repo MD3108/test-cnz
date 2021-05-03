f8cb5e5efd2b27936217eca0a6479f8f090e30f5
MD3108 <pinho_daniel098@hotmail.fr>
2021-05-03 14:36:59 +0200

    Some styling on from before working on store function in CRUD controller Note

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -20,12 +20,42 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <!--Warning-->
+            <symbol id="icon-warning" viewBox="0 0 10 32">
+                <path d="M0.594 27.459c0-2.25 1.824-4.074 4.074-4.074s4.074 1.824 4.074 4.074c0 2.25-1.824 4.074-4.074 4.074s-4.074-1.824-4.074-4.074z"></path>
+                <path d="M4.669 20.839v0c-2.25 0-4.074-1.824-4.074-4.074v-12.223c0-2.25 1.824-4.074 4.074-4.074v0c2.25 0 4.074 1.824 4.074 4.074v12.223c0 2.25-1.824 4.074-4.074 4.074z"></path>
+            </symbol>
+        <!-- Next -->
+            <symbol id="icon-next" viewBox="0 0 19 32">
+                <path d="M18.171 17.49c0.815-0.815 0.815-2.136 0-2.951l-13.281-13.281c-0.815-0.815-2.136-0.815-2.951 0s-0.815 2.136 0 2.951l11.806 11.806-11.806 11.805c-0.815 0.815-0.815 2.136 0 2.951s2.136 0.815 2.951 0l13.281-13.281zM15.304 18.101h1.391v-4.174h-1.391v4.174z"></path>
+            </symbol>
+            <!-- Previous -->
+            <symbol id="icon-previous" viewBox="0 0 31 32">
+                <path d="M7.453 14.431c-0.735 0.735-0.735 1.927 0 2.662l11.979 11.979c0.735 0.735 1.927 0.735 2.662 0s0.735-1.927 0-2.662l-10.648-10.648 10.648-10.648c0.735-0.735 0.735-1.927 0-2.662s-1.927-0.735-2.662 0l-11.979 11.979zM9.412 13.88h-0.627v3.765h0.627v-3.765z"></path>
+            </symbol>
+         <!-- Edit -->
+            <symbol id="icon-edit" viewBox="0 0 34 32">
+                <path d="M26.767 1.908l4.174 4.173c0.347 0.347 0.347 0.91 0 1.257l-15.035 15.035c-0.347 0.347-0.91 0.347-1.257 0l-4.173-4.174c-0.347-0.347-0.347-0.91 0-1.257l15.035-15.035c0.347-0.347 0.91-0.347 1.257 0z"></path>
+                <path d="M6.355 27.749c-0.661 0.177-1.266-0.428-1.089-1.089l1.498-5.592c0.177-0.661 1.003-0.882 1.487-0.398l4.093 4.093c0.484 0.484 0.262 1.31-0.399 1.487l-5.592 1.498z"></path>
+            </symbol>
+        <!-- Close -->
+            <symbol id="icon-close" viewBox="0 0 32 32">
+                <path d="M21.802 3.508c1.848-1.848 4.845-1.848 6.693 0s1.848 4.845 0 6.693l-18.406 18.406c-1.848 1.848-4.845 1.848-6.693 0s-1.848-4.845 0-6.693l18.406-18.406z"></path>
+                <path d="M28.414 21.86c1.846 1.85 1.846 4.85 0 6.7s-4.839 1.85-6.686 0l-18.386-18.426c-1.846-1.85-1.846-4.85 0-6.7s4.84-1.85 6.686 0l18.386 18.426z"></path>
+            </symbol>
+         <!-- Check -->
+            <symbol id="icon-check" viewBox="0 0 37 32">
+                <path d="M4.976 25.622l28.732-16.86 2.986 5.256-28.732 16.86-2.986-5.256z"></path>
+                <path d="M5.172 13.827l4.977 8.761-5.172 3.035-4.977-8.761 5.172-3.035z"></path>
+            </symbol>
+        </svg>
     <div id="app">
         <main class="py-4">
             <section class="section --form">
                 <div class="container">
                     <div class="row">
-                        <div class="col">
+                        <div class="col-9">
                             <h1 class="title --main text-center">
                                 Create your Combo Note
                             </h1>
@@ -45,19 +75,19 @@
                                 action="/note" 
                                 method="POST">
                                 @csrf
-                                <div class="form__part --fighter">
+                                <div class="form__part --fighter" data-type="fighter">
                                     <div>
                                         <h2 class="title --form">
                                             Who’s your fighter
                                         </h2>
                                     </div>
-                                    <div class="form__fighters mb-3">
+                                    <div class="form__fighters">
                                         <div class="fighters__select">
                                             @foreach ($fighters as $fighter)
                                             <label class="f-select__fighter" for="{{ $fighter->id }}" >
                                                 <img src="{{ asset('/storage' .$fighter->image_path) }}" alt="{{ $fighter->name }}">
                                             </label>
-                                            <input type="checkbox" name="fighter" id="{{ $fighter->id }}">
+                                            <input type="checkbox" name="{{ $fighter->name }}" id="{{ $fighter->id }}">
                                             @endforeach
                                         </div>
                                        
@@ -71,19 +101,19 @@
                                                     <label class="f-select__fighter" for="chosen-1" >
                                                         <img src="#" alt="user select 1">
                                                     </label>
-                                                    <input type="checkbox" name="chosen" id="chosen-1">
+                                                    <input type="checkbox" name="chosen-1" id="chosen-1">
                                                 </div>
                                                 <div class="f-chosen__el --a1">
                                                     <label class="f-select__fighter" for="chosen-2" >
                                                         <img src="#" alt="user select 2">
                                                     </label>
-                                                    <input type="checkbox" name="chosen" id="chosen-2">
+                                                    <input type="checkbox" name="chosen-2" id="chosen-2">
                                                 </div>
                                                 <div class="f-chosen__el --a2">
                                                     <label class="f-select__fighter" for="chosen-3" >
                                                         <img src="#" alt="user select 3">
                                                     </label>
-                                                    <input type="checkbox" name="chosen" id="chosen-3">
+                                                    <input type="checkbox" name="chosen-3" id="chosen-3">
                                                 </div>
                                             </div>
                                         </div>
@@ -92,7 +122,7 @@
                                         </button>
                                     </div>
                                 </div>
-                                <div class="form__part --combo">
+                                <div class="form__part --combo" data-type="combo">
                                     <div class="mb-3">
                                         <label for="name" class="form-label">
                                             Give your note a Name
@@ -107,8 +137,11 @@
                                             <img src="{{ asset('/storage'. '/images/buttons/L.png') }}" alt="L">
                                         </div>
                                     </div>
+                                    <button class="btn btn-primary mb-3" type="submit">
+                                        Next
+                                    </button>
                                 </div>
-                                <div class="form__part --details">
+                                <div class="form__part --details" data-type="details">
                                     <div class="mb-3">
                                         <label for="damage" class="form-label">
                                             How  much Damage is inflicted
@@ -132,26 +165,56 @@
                                     <div>
                                         <div>
                                             @foreach ($categories as $category)
-                                            <label class="" for="{{ $category->name }}" >
-                                                {{ $category->name }}
-                                            </label>
-                                            <input type="checkbox" name="category" id="{{ $category->name }}">
+                                            <div>
+                                                <label class="" for="{{ $category->name }}" >
+                                                    {{ $category->name }}
+                                                </label>
+                                                <input type="checkbox" name="category" id="{{ $category->name }}">
+                                            </div>
                                             @endforeach
                                         </div>
                                     </div>
                                     <div>
                                         <div>
-                                            <!--
-                                            <label class="" for="{# $difficulty->name }}" >
-                                                {# $difficulty->name }}
+                                            <label class="" for="easy" >
+                                                Easy
                                             </label>
-                                            <input type="checkbox" name="category" id="{# $difficulty->name }}">
-                                        -->
+                                            <input type="checkbox" name="easy" id="easy">
+                                        </div>
+                                        <div>
+                                            <label class="" for="Average" >
+                                                Average
+                                            </label>
+                                            <input type="checkbox" name="Average" id="Average">
+                                        </div><div>
+                                            <label class="" for="Hard" >
+                                                Hard
+                                            </label>
+                                            <input type="checkbox" name="Hard" id="Hard">
+                                        </div>
+                                        <div>
+                                            <!--Scarlet-photon LvL-->
+                                            <label class="" for="Expert" >
+                                                Expert
+                                            </label>
+                                            <input type="checkbox" name="Expert" id="Expert">
                                         </div>
                                     </div>
+                                    <button class="btn btn-primary mb-3" type="submit">
+                                        Next
+                                    </button>
                                 </div>
 
-                                <div class="form__part --check">
+                                <div class="form__part --check" data-type="check">
+                                    <div>
+                                        <div>
+                                            
+                                        </div>
+                                    </div>
+
+                                    <button class="btn btn-primary mb-3" type="submit">
+                                        Next
+                                    </button>
                                     
                                 </div>
 
@@ -159,6 +222,39 @@
                                     <button type="submit" class="btn btn-primary mb-3">Publish</button>
                                 </div>
                             </form>
+                        </div>
+                        <div class="col-3">
+                            <button class="btn-close --vod" aria-label="Close" >
+                                <svg class="icon icon-close">
+                                    <use xlink:href="#icon-close"></use>
+                                </svg>
+                            </button>
+                            <div class="progression">
+                                <div>
+                                    <h2 class="title --progression">
+                                        Note Completion
+                                    </h2>
+                                    <ul class="">
+                                        <li>
+                                            Fighter choice
+                                        </li>
+                                        <li>
+                                            The combo
+                                        </li>
+                                        <li>
+                                            Combo details
+                                        </li>
+                                        <li>
+                                            Check up
+                                        </li>
+                                    </ul>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                                            0%
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
